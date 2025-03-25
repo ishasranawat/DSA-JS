8e5128aa657839a9f3113c96dc0b4be8b3a763e2
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-03-25 12:14:08 +0530

    Update Easy.js

--- Loops/Easy.js
+++ Loops/Easy.js
@@ -33,6 +33,70 @@ while(i<myArr.length){
   i++;
 }
 
+//Print numbers from 1 to N using a loop.
+
+for(let i=1; i<=10; i++){
+  console.log(i);
+}
+
+//Print numbers from N to 1 using a loop.
+for(let i=10; i>=1; i--){
+  console.log(i);
+}
+
+//Find the sum of digits of a given number.
+let sum=0;
+let num=123;
+num= String(num)
+for (let i=0; i<num.length; i++){
+  sum= sum+  Number(num[i]);
+}
+console.log(sum);
+
+
+//Count the number of digits in a number.
+
+let count=0;
+let digit=12345678;
+digit= String(digit)
+for(let i=0; i<digit.length; i++){
+  count++;
+}
+console.log(count);
+
+
+//Find the sum of all even digits in a number
+let sum=0;
+let num=123456789;
+num=String(num);
+for(let i=0; i<num.length; i++){
+  let digit= Number(num[i]);
+    if(digit%2==0){
+      sum+=digit;
+    }
+}
+console.log(sum);
+
+
+
+
+
+
+
+// Find the sum of all odd digits in a number.
+
+let sum=0;
+let num= 123456789;
+num=String(num);
+for(let i=0; i<num.length; i++){
+  let digit= Number(num[i]);
+  if( digit%2!=0){
+    sum+= digit;
+  }
+}
+console.log(sum);
+
+
 
 
 
