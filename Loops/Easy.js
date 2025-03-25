acb54d2181bd143fb2a59e6515fdddc37eddcf81
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-03-25 11:33:11 +0530

    Update Easy.js

--- Loops/Easy.js
+++ Loops/Easy.js
@@ -1,3 +1,45 @@
+//Print all odd numbers from 1 to 15
+for(let i=1; i<16; i=i+2){
+  console.log(i);
+}
+
+//Print even  numbers from 2 to 10
+for( let i=0; i<=10; i=i+2){
+  console.log(i);
+}
+
+//Table of 5
+
+//approach1
+for(let i=5,j=1; j<=10; j++){
+  console.log(`${i}*${j}=${i}`)
+}
+//2
+for(let i=5; i<=50; i+=5){
+  console.log(i);
+}
+//3
+let n= prompt("Enter your number: ");
+n=parseInt(n);
+for(i=n; i<=n*10; i=i+n){
+  console.log(i);
+}
+
+//while
+let myArr= ["one", "two", "three", "four"]
+let i=0;
+while(i<myArr.length){
+  console.log(`The value is ${myArr[i]}`);
+  i++;
+}
+
+
+
+
+
+
+
+
 
 
 1️⃣ Print numbers from 1 to N using a loop.
